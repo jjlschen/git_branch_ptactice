93985288152a1b4210aa75d7b6bf25d6cba33eea
jjlschen <jjlschen@yahoo.com.tw>
2019-05-18 13:56:23 +0800

    Test power function from <math.h>...

--- main.c
+++ main.c
@@ -2,17 +2,18 @@
 # include <stdlib.h>
 # include <stdio.h>
 
-
+# include <math.h>
 
 
 int main()
 {
 	printf("Hello World!");
 	
-	
+	double a = 1.6;
+	printf("\n%.2f^2 = %.2f", a, pow(a,2));
 
-	
 	system("pause");
 	
 	return 0;
 }
+
